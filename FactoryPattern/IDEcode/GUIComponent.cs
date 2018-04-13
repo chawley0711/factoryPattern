92dcc6e91d2646be69371385b04a238761d68636
Unknown <CHawley@student.neumont.edu>
2018-04-13 13:50:52 -0600

    GUIComponent setup finish

---
+++ FactoryPattern/IDEcode/GUIComponent.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDEcode
+{
+    public class GUIComponent
+    {
+        private int left;
+        private int top;
+        private int width;
+        private int height;
+
+        public void initialize(int l, int t, int w, int h)
+        {
+            this.left = l;
+            this.top = t;
+            this.width = w;
+            this.height = h;
+        }
+    }
+}
--- FactoryPattern/IDEcode/HTMLComponent.cs
+++ FactoryPattern/IDEcode/HTMLComponent.cs
@@ -6,8 +6,8 @@ using System.Threading.Tasks;
 
 namespace IDEcode
 {
-     public class GUIComponentFactory
-     {
-        
-     }
+    class HTMLComponent : GUIComponent
+    {
+
+    }
 }
