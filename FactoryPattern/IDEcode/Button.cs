ee48c8e5b30c7aef2c284237289ec69dc7d680c8
Tiln <tiln141@gmail.om>
2018-04-16 10:51:30 -0600

    added button and other thing to project

---
+++ FactoryPattern/IDEcode/Button.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDEcode
+{
+    public class Button : GUIComponent
+    {
+        private string text;
+        private int fontsize;
+
+        public Button(string text, int fontsize)
+        {
+            this.text = text;
+            this.fontsize = fontsize;
+        }
+    }
+}
---
+++ FactoryPattern/IDEcode/Label.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDEcode
+{
+    public class Label
+    {
+        private string text;
+        private int fontsize;
+
+        public Label(string text, int fontsize)
+        {
+            this.text = text;
+            this.fontsize = fontsize;
+        }
+    }
+}
